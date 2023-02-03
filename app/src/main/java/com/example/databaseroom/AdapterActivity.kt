c247d4626d179d8a3f591461438665af9bfba5d9
Maula <maula.minhatul@gmail.com>
2023-02-03 09:47:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/databaseroom/AdapterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.databaseroom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdapterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adapter)
+    }
+}
